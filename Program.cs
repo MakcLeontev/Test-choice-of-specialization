b66a6f1926663c297efdce8917c53f015a184475
maximleontev <lmaks21@mail.ru>
2022-08-09 21:39:06 +0300

    write Readme

--- Program.cs
+++ Program.cs
@@ -1,20 +1,19 @@
 ﻿internal class GeekBrains
 {
     //задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
-    //длина которых меньше либо равна 3 символа. Первоначальный массив нужно ввести с клавиатуры, 
+    //длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
     //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться 
     //коллекциями лучше обойтись исключительно массивами. 
     private static void Main(string[] args)
     {
-        Console.Write("Введите количество строк в массиве: ");
+        Console.Write("Введите количество элементов в массиве: ");
         int size = Convert.ToInt32(Console.ReadLine());
         string[] firstArray = new string[size];
-        string[] secondArray = new string[size];
         FillArrayConsole(firstArray);
         PrintArray(SortArray(firstArray));
 
     }
-    public static void FillArrayConsole(string[] array)
+    public static void FillArrayConsole(string?[] array)
     {
         for (int i = 0; i < array.Length; i++)
         {
@@ -33,8 +32,8 @@
                 count++;
             }
         }
-        string [] secondArray= new string[count];
-        int secondArrayPosition=0;
+        string[] secondArray = new string[count];
+        int secondArrayPosition = 0;
         for (int i = 0; i < firstArray.Length; i++)
         {
             if (firstArray[i].Length <= 3)
@@ -44,16 +43,12 @@
             }
         }
         return secondArray;
-         
+
     }
     public static void PrintArray(string[] array)
     {
         Console.WriteLine("Итоговый массив");
-        // for (int i = 0; i < array.Length; i++)
-        // {
-        //     Console.WriteLine( array[i]);
-        // }
-        Console.WriteLine( "{"+string.Join("," , array)+"}");
+        Console.WriteLine("[" + string.Join(",", array) + "]");
     }
 
 }
